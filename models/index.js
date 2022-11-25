15401c17f2c753d146f8e154ab7fed4c39fe9765
Hyomk <qet557@gmail.com>
2022-11-25 20:17:34 +0900

    fix : env error

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+const config = require(__dirname + "/../config/config.js").development;
 const db = {};
 
 const Host = require("./host");
@@ -13,16 +13,12 @@ const Guest = require("./guest");
 const Quiz = require("./quiz");
 
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(
-        config.database,
-        config.username,
-        config.password,
-        config
-    );
-}
+sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
